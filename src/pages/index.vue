8a6f45612af33d3c00deeb158aef973db3db3db3
ozelotjp <contact@ozelot.app>
2020-04-13 16:39:25 +0000

    initialize commit

---
+++ src/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>RefEmitWithCompositionAPI</h1>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {
+    const message = 'hogehoge'
+    return {
+      message
+    }
+  }
+})
+</script>
